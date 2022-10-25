570fd0b6295b2a0d92084544f88390a25f022f7d
Your Name <you@example.com>
2022-10-25 21:07:50 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-messageOne.textContent = 'from Javascript'
+// messageOne.textContent = 'from Javascript'
 
 weatherForm.addEventListener('submit', (e)=> {
     e.preventDefault()
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e)=> {
     messageOne.textContent = 'Loading....'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if(data.error) {
             messageOne.textContent = data.error
